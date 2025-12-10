7d6771f7eb4c4c486485c03cf4249d5a104d5837
Julie Sofie <106279190+julsofpet@users.noreply.github.com>
2025-12-10 21:13:55 +0100

    change order of the kde plots

--- assets/js/story-script.js
+++ assets/js/story-script.js
@@ -531,8 +531,8 @@ function initObserver() {
                         
                         setTimeout(() => {
                             // 6. KDE Plot appare
-                            revealElement('s8-kde-comp-row');
                             revealElement('s8-kde-dist-row');
+                            revealElement('s8-kde-comp-row');
                             
                             setTimeout(() => {
                                 // 7. Scienziato (Risultati P-Value)
